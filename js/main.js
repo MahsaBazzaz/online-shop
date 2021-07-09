612934de301a5c08187d980e17f258a50e84b973
Mahsa <m@cirqle.games>
2021-07-09 13:55:05 +0430

    revert slider changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-var slideIndex = 0;
+// var slideIndex = 0;
 
-window.onload = function() {
+// window.onload = function() {
 
-    document.getElementsByClassName("prev")[0].addEventListener("click", plusSlides(-1));
-    document.getElementsByClassName("next")[0].addEventListener("click", plusSlides(1));
+//     document.getElementsByClassName("prev")[0].addEventListener("click", plusSlides(-1));
+//     document.getElementsByClassName("next")[0].addEventListener("click", plusSlides(1));
 
-    window.setInterval(showSlides, 3000);
+//     window.setInterval(showSlides, 3000);
 
-    function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) { slideIndex = 1 }
-        slides[slideIndex - 1].style.display = "block";
-        setTimeout(showSlides, 2000); // Change image every 2 seconds
-    }
+//     function showSlides() {
+//         var i;
+//         var slides = document.getElementsByClassName("mySlides");
+//         for (i = 0; i < slides.length; i++) {
+//             slides[i].style.display = "none";
+//         }
+//         slideIndex++;
+//         if (slideIndex > slides.length) { slideIndex = 1 }
+//         slides[slideIndex - 1].style.display = "block";
+//         setTimeout(showSlides, 2000); // Change image every 2 seconds
+//     }
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
-}
+//     function plusSlides(n) {
+//         showSlides(slideIndex += n);
+//     }
+// }
