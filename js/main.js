5e2004a9fa438c2f540b94cbe55372ac5c0b6a35
mahsabazzaz <mahsa.bazzaz@gmail.com>
2021-07-10 19:37:08 +0430

    added sorting box script which was left behind

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+//
+// hero header
+//
 var slideIndex = 0;
 var slides = document.getElementsByClassName("slider-image");
 showSlide(0);
 
-// hero header
 window.setInterval(function() {
     slideIndex++;
     if (slideIndex > slides.length)
@@ -30,8 +32,23 @@ function showSlide(index) {
     }
     slides[index].style.display = "inline-block";
 }
+//
+// sorting box
+//
+window.onload = function() {
+    document.getElementById("best-seller").addEventListener("click", sortByBestSeller);
+    document.getElementById("price").addEventListener("click", sortByBestSeller);
+}
 
+function sortByBestSeller() {
+    document.getElementById("best-seller").classList.toggle('sorting-box-btn-active');
+    document.getElementById("best-seller").classList.toggle('sorting-box-btn-deactive');
+    document.getElementById("price").classList.toggle('sorting-box-btn-active');
+    document.getElementById("price").classList.toggle('sorting-box-btn-deactive');
+}
+//
 // modal
+//
 var modal = document.getElementById("modal");
 var btn = document.getElementsByClassName("dropdownbtn")[0];
 var span = document.getElementsByClassName("close")[0];
@@ -41,6 +58,7 @@ btn.onclick = function() {
 span.onclick = function() {
         modal.style.display = "none";
     }
+    //FIXME: the exit modal by clicking on anything rather than itself doesn't work
     // window.onclick = function(event) {
     //     if (event.target.id != modal.id && event.target.id != btn.id) {
     //         modal.style.display = "none";
