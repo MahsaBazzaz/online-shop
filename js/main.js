3f8f0bcdb9d09d988ccddc71408461c639b55d88
mahsabazzaz <mahsa.bazzaz@gmail.com>
2021-07-10 17:17:17 +0430

    scaffolded modal, it has a bug

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var slideIndex = 0;
 var slides = document.getElementsByClassName("slider-image");
 showSlide(0);
 
-
+// hero header
 window.setInterval(function() {
     slideIndex++;
     if (slideIndex > slides.length)
@@ -29,4 +29,15 @@ function showSlide(index) {
         slides[i].style.display = "none";
     }
     slides[index].style.display = "inline-block";
+}
+
+// modal
+var modal = document.getElementsByClassName("modal")[0];
+var btn = document.getElementsByClassName("dropdownbtn")[0];
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+span.onclick = function() {
+    modal.style.display = "none";
 }
