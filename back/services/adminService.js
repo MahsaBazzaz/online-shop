59e479aae8b6e4417aba6ca2ae61adb172636d2e
mhnaderi99 <mhnaderi99@gmail.com>
2021-07-13 17:17:40 +0430

    added user and admin services signatures

---
+++ back/services/adminService.js
@@ -0,0 +1,29 @@
+
+
+function editProduct(productId, newFields) {
+    
+}
+
+function getAllReceipts() {
+    
+}
+
+function searchReceiptByTackingCode(trackingCode) {
+    
+}
+
+function changeReceiptStatus(receiptId, newStatus) {
+    
+}
+
+function createCategory(newCategory) {
+    
+}
+
+function editCategory(categoryId, newFields) {
+    
+}
+
+function deleteCategory(categoryId) {
+    
+}
