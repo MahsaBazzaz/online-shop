cec8a63fac6680abe32aeb0fc5d612e5629e5bd4
mahsabazzaz <mahsa.bazzaz@gmail.com>
2021-07-13 18:43:27 +0430

    admin service completed

--- back/services/adminService.js
+++ back/services/adminService.js
@@ -4,29 +4,39 @@ const Receipt = require('./db/receipts');
 
 
 async function editProduct(productId, newFields) {
-    await Product.editProduct(newFields, productId);
+    return await Product.editProduct(newFields, productId);
 }
 
 async function getAllReceipts() {
-    await Receipt.getAllReceipts();
+    return await Receipt.getAllReceipts();
 }
 
 async function searchReceiptByTackingCode(trackingCode) {
-    await Receipt.findReceiptByTrackingCode(trackingCode);
+    return await Receipt.findReceiptByTrackingCode(trackingCode);
 }
 
 async function changeReceiptStatus(receiptId, newStatus) {
-    await Receipt.editReceipt({ "status": newStatus }, receiptId);
+    return await Receipt.editReceipt({ "status": newStatus }, receiptId);
 }
 
 async function createCategory(newCategory) {
-    await Category.createCategory(newCategory);
+    return await Category.createCategory(newCategory);
 }
 
 async function editCategory(categoryId, newFields) {
-    await Category.editCategory(newFields, categoryId);
+    return await Category.editCategory(newFields, categoryId);
 }
 
 async function deleteCategory(categoryId) {
-    await Category.deleteCategory(categoryId);
+    return await Category.deleteCategory(categoryId);
 }
+
+module.exports = {
+    editProduct,
+    getAllReceipts,
+    searchReceiptByTackingCode,
+    changeReceiptStatus,
+    createCategory,
+    editCategory,
+    deleteCategory
+};
